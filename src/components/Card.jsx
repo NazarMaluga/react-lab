22e84548482214351e39fa3512bb66e8c61d198e
Nazar Maluga <nazar.maluga@example.com>
2025-04-01 20:36:27 +0300

    Updated Card component to use MUI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import React from "react";
 import "./Card.css";
 
@@ -5,9 +7,19 @@ const Card = ({ title, description, image }) => {
   return (
     <div className="card">
       <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <button onClick={() => alert(title)}>Дізнатися більше</button>
+      <Typography variant="h5" component="h3" gutterBottom>
+        {title}
+      </Typography>
+      <Typography variant="body1" paragraph>
+        {description}
+      </Typography>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        onClick={() => alert(title)}
+      >
+        Дізнатися більше
+      </Button>
     </div>
   );
 };
